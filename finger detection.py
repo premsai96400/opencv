848716aeb2c93b15b795a516001300ad122f7f20
Saikiran727 <60846827+Saikiran727@users.noreply.github.com>
2020-10-04 13:46:31 +0530

    Add files via upload

---
+++ finger detection.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+def main():
+
+    cap = cv2.VideoCapture(0)
+    
+    if cap.isOpened():
+         ret,frame = cap.read()
+    else:
+         ret = False
+
+
+    while ret:
+        ret, frame = cap.read()
+        cv2.rectangle(frame,(50,50),(450,450),(255,0,0),1)
+        a = frame[50:450,50:450]
+        b = cv2.cvtColor(a,cv2.COLOR_BGR2GRAY)
+        c = cv2.blur(b,(3,3))
+        ret,d = cv2.threshold(c, 100, 255, cv2.THRESH_BINARY_INV)        
+        x,hierarchy = cv2.findContours(d,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for i in range(len(x)):
+            hull = cv2.convexHull(x[i])
+            cv2.drawContours(d, [hull], -1, (255, 0, 0), 2)
+        cv2.imshow('preview',frame)
+        cv2.imshow('interest',d)
+        print(x)
+        if cv2.waitKey(1) == 27: 
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+    
+if __name__ == "__main__":
+    main()
